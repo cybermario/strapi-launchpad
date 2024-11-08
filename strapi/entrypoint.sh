c9527f41250d453b3a8c08a297e32b3c25c24a25
Mario Bodenmüller <mario.bodenmueller@gmail.com>
2024-11-08 23:17:25 +0100

    import of data configurable

--- strapi/entrypoint.sh
+++ strapi/entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 echo "Waiting for database to be set up..."
-# Run migrations or seeding only if necessary
-sleep 55 && echo "Running database seed..." && echo "y" | yarn seed
+
+# Wait for the database to be ready
+sleep 55
+
+# Check if IMPORT_DATA is set to "true" before running the seed command
+if [ "$IMPORT_DATA" = "true" ]; then
+  echo "IMPORT_DATA is set to true. Running database seed..."
+  echo "y" | yarn seed
+else
+  echo "IMPORT_DATA is not set to true. Skipping database seed."
+fi
+
 # Start the application
 yarn develop
